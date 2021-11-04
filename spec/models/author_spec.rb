708881b0eeddbe706c24d9053ebdbe090eee6ca5
laugengebaeck <lukasrost2@gmail.com>
2021-11-04 21:19:32 +0100

    Failing test for #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,4 +7,9 @@ require 'rails_helper'
         expect(author.last_name).to eq("Turing")
         expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
     end
+
+    it "should have a method returning the full name" do
+        author = Author.new(:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing")
+        expect(author.name).to eq("Alan Turing")
+    end
  end
