663548ca5cda282c853647388f8836f00c51ef6b
laugengebaeck <lukasrost2@gmail.com>
2021-11-04 21:10:11 +0100

    Failing test for #4

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+ describe "Add Author", type: :model do
+    it "should create an author from given parameters" do
+        author = Author.new(:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing")
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    end
+ end
