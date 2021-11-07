0d6b8b3a260cc65e285823b9f851ea0b9e46f6fc
laugengebaeck <lukasrost2@gmail.com>
2021-11-07 09:41:52 +0100

    Failing test for #9

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
- describe "Add Author", type: :model do
+ describe "Author", type: :model do
     it "should create an author from given parameters" do
         author = Author.new(:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing")
         expect(author.first_name).to eq("Alan")
@@ -12,5 +12,9 @@ require 'rails_helper'
         author = Author.new(:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing")
         expect(author.name).to eq("Alan Turing")
     end
-    
+
+    it "should not validate without last name" do
+        author = Author.new(:first_name => "Alan", :last_name => "", :homepage => "http://example.com")
+        expect(author).to_not be_valid
+    end
  end
