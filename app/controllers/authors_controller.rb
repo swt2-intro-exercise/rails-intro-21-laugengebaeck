134f8c709525719431422dd96e8cac66147dbd5f
laugengebaeck <lukasrost2@gmail.com>
2021-11-07 08:39:45 +0100

    Fix for #6 (add author create method)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,13 @@ class AuthorsController < ApplicationController
     end
 
     def create
-        render plain: params[:author].inspect
+        @author = Author.new(author_params)
+        @author.save
+        redirect_to root_path, notice: 'Success!'
     end
+
+    private
+        def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
