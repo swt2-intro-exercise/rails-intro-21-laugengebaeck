aea993b8cfe2b2f2437ee0f1752e8ad6685469e6
laugengebaeck <lukasrost2@gmail.com>
2021-11-07 08:55:02 +0100

    Fix #7 (create show page)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,15 @@
 class AuthorsController < ApplicationController
+    def show
+        @author = Author.find(params[:id])
+    end
+
     def new
     end
 
     def create
         @author = Author.new(author_params)
         @author.save
-        redirect_to root_path, notice: 'Success!'
+        redirect_to @author
     end
 
     private
