0680d9e1014132d0dd99235ea0c8cb8f18ceefaf
laugengebaeck <lukasrost2@gmail.com>
2021-11-07 09:26:11 +0100

    Failing test for #8

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+ describe "Author index page", type: :feature do
+   it "should exist at 'authors_path' and render withour error" do
+     @alan = FactoryBot.create :author
+     visit authors_path
+     expect(page).to have_text(@alan.first_name)
+     expect(page).to have_text(@alan.last_name)
+     expect(page).to have_text(@alan.homepage)
+     expect(page).to have_link 'Show', href: author_path(@alan)
+   end
+
+   it "should contain a link to add new authors" do
+    visit authors_path
+    expect(page).to have_link 'New', href: new_author_path
+   end
+ end
