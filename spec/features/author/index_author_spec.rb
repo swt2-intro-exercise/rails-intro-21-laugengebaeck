d4dca42593dc2c46300dda572ed9849399b05895
laugengebaeck <lukasrost2@gmail.com>
2021-11-08 11:54:31 +0100

    Failing test for #11

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -14,4 +14,19 @@ describe 'Author index page', type: :feature do
    visit authors_path
    expect(page).to have_link 'New', href: new_author_path
   end
+  
+  it 'should have a link to delete an author' do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Delete', href: author_path(@alan)
+  end
+
+  it 'should delete an author on clicking the link' do
+    @alan = FactoryBot.create :author
+    old_count = Author.count
+    visit authors_path
+    find("a[data-method='delete'][href='#{author_path(@alan)}']").click
+    new_count = Author.count
+    expect(new_count).to be < old_count
+  end
 end
