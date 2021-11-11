7d88e075f68724459da84a8b65ac8e1cd765f1cc
laugengebaeck <lukasrost2@gmail.com>
2021-11-11 18:27:10 +0100

    Fix #15 (add authors to paper detail page)

--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :paper do
-    title { "MyString" }
-    venue { "MyString" }
-    year { 1 }
+    title { "Computing Machinery and Intelligence" }
+    venue { "mind 49: 433-460" }
+    year { 1950 }
   end
 end
