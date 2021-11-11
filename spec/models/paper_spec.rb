3c822e381787f0b70eba8c0f4f7b13511c9bccb4
laugengebaeck <lukasrost2@gmail.com>
2021-11-11 17:36:18 +0100

    Failing test for #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not validate without title' do
+    paper = Paper.new(title: nil, venue: "mind 49: 433-460", year: 1950)
+    expect(paper).to_not be_valid
+  end
+
+  it 'should not validate without venue' do
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: nil, year: 1950)
+    expect(paper).to_not be_valid
+  end
+
+  it 'should not validate without year' do
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "mind 49: 433-460", year: nil)
+    expect(paper).to_not be_valid
+  end
+
+  it 'should not validate with non-numerical year' do
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "mind 49: 433-460", year: "1950A")
+    expect(paper).to_not be_valid
+  end
 end
